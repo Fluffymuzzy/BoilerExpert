cd6f1da57ffdb05d28c1e5d7e461b366be0c46f7
Fluffymuzzy <Fluffymuzzy1@icloud.com>
2022-06-19 13:18:17 +0300

    redesigned the plugin and added a form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Connect EXPRESS
 const express = require("express");
 const app = express();
+// const router = express.Router();
 
 // Connect DATABASE
 const mySql = require("mysql");
@@ -9,7 +10,13 @@ const mySql = require("mysql");
 const port = 3000;
 const hostname = "127.0.0.1";
 
-app.listen(port, () => {
+// module.exports = router;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("there was a problem", err);
+    return;
+  }
   console.log(`Server started on http://${hostname}:${port}`);
 });
 
@@ -33,15 +40,24 @@ app.set("view engine", "pug");
 
 // MAIN PAGE
 app.get("/", (req, res) => {
-  res.render("main");
+  let goods = new Promise((resolve, reject) => {
+    conn.query("SELECT * FROM goods ORDER BY id DESC LIMIT 3", (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+  Promise.all([goods]).then((value) => {
+    console.log(value[0]);
+    res.render("main", {
+      goods: value[0],
+    });
+  });
 });
 
-// EXCELLENCE PAGE
-app.get("/service", (req, res) => {
-  res.render("service")
+// CATALOG PAGE
+app.get("/catalogPage", (req, res) => {
+  res.send("<h1>PIZDA<h1/>");
 });
-
-// CATALOG PAGE 
 
 // CALLBACK FORM
 app.get("/callbackForm", (req, res) => {
@@ -63,10 +79,11 @@ app.post("/finish-callback", function (req, res) {
 
 app.get("/admin-callbacks", (req, res) => {
   conn.query(
-    `SELECT id,username,surname,number,FROM_UNIXTIME(date, '%D %M %Y %H:%i:%s') as unix_timestamp 
-     FROM callback
-     ORDER BY id DESC
-  `,
+    `
+    SELECT id,username,surname,number,FROM_UNIXTIME(date, '%D %M %Y %H:%i:%s') as unix_timestamp 
+    FROM callback
+    ORDER BY id DESC
+    `,
     function (err, result) {
       if (err) throw err;
       res.render("adminTable", {
@@ -76,11 +93,11 @@ app.get("/admin-callbacks", (req, res) => {
   );
 });
 
-// BTN
+// BTNS
 
 app.get("/btn-reset", (req, res) => {
   conn.query(
-  `
+    `
   TRUNCATE TABLE callback
   `,
     function (err, result) {
@@ -89,3 +106,7 @@ app.get("/btn-reset", (req, res) => {
     }
   );
 });
+
+app.get("");
+
+// ****************************************************************
--- public/js/callbackForm.js
+++ public/js/callbackForm.js
@@ -1,33 +1,55 @@
-const firstName = document.querySelector("#name");
-const surname = document.querySelector("#surname");
-const number = document.querySelector("#phoneNumber");
-const form = document.querySelector(".needs-validation");
+// const name = document.querySelector("#name");
+// const surname = document.querySelector("#surname");
+// const number = document.querySelector("#phoneNumber");
+const forms = document.querySelector(".needs-validation");
 
+const form = document.createElement("form");
+form.innerHTML = `
+<div class="popup-container">
+  <div class="popup-wrapper">
+      <div class="col-md-12">
+        <label class="form-label" for="name">Имя</label>
+        <input class="form-control" type="text" id="name" placeholder="Имя" required="required"/>
+        <div class="invalid-feedback">Пожалуйста, укажите корректно имя!</div>
+      </div>
+      <div class="col-md-12">
+        <label class="form-label" for="phoneNumber">Номер телефона</label>
+        <input class="form-control" type="tel" minlength="11" id="phoneNumber" placeholder="+38(000)000-00-00" required="required"/>
+        <div class="invalid-feedback">Пожалуйста, укажите корректно номер телефона!</div>
+      </div>
+  </div>
+</div>
+`;
 
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (!form.checkValidity()) {
-    event.stopPropagation();
-    form.classList.add("was-validated");
-  } else {
-    fetch("/finish-callback", {
-      method: "POST",
-      body: JSON.stringify({
-        name: firstName.value.trim(),
-        surname: surname.value.trim(),
-        number: Number(number.value),
-      }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+/* It's a function that opens a modal window with a form. */
+const openCallbackForm = document.querySelector(".callback-btn");
+openCallbackForm.addEventListener("click", function () {
+  swal({
+    title: "Вам брякнуть?",
+    icon: "",
+    content: form,
+    buttons: {
+      cancel: {
+        text: "Cancel",
+        value: null,
+        visible: true,
+        className: "",
+        closeModal: true,
       },
-    }).then(function (response) {
-      return response.text();
-    }).then(function(body){
-        if(body==='1'){
-            console.log('done');
-        }
-    })
-  }
+      confirm: {
+        text: "OK",
+        value: true,
+        visible: true,
+        className: "",
+        closeModal: false,
+      },
+    },
+  }).then((confirm) => {
+    if (confirm) {
+        // ???
+    }
+    else {
+        // ???
+    }
+});
 });
