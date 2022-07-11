4c4bc88ae18f9393a1bbbaee2e39fe738e9cf018
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2022-07-11 17:27:32 +0300

    add calculation items to shopping cart

--- public/js/shoppingCartFormation.js
+++ public/js/shoppingCartFormation.js
@@ -2,9 +2,6 @@ import { fetchData } from "./fetch.js";
 // window.onload = () => {
 let cart = {};
 
-/* Listening for a click event on the document. If the click event is on an element with the class
-add-to-cart, it will call the addToCart function. If the click event is on an element with the class
-remove-from-cart, it will call the removeFromCart function. */
 document.addEventListener("click", (event) => {
   if (event.target.classList.contains("add-to-cart")) {
     addToCart(event.target);
@@ -13,20 +10,13 @@ document.addEventListener("click", (event) => {
   }
 });
 
-/* Checking if there is a cart in local storage. If there is, it will parse the cart and call the
-ajaxGetGoodsInfo function. */
 if (localStorage.getItem("cart")) {
   cart = JSON.parse(localStorage.getItem("cart"));
   ajaxGetGoodsInfo();
 }
 
-/**
- * If the item is already in the cart, increment the quantity, otherwise add it to the cart.
- * @param item - the item that was clicked on
- */
 function addToCart(item) {
   let dataId = item.dataset.id;
-
   if (cart[dataId]) {
     cart[dataId]++;
   } else {
@@ -35,13 +25,8 @@ function addToCart(item) {
   ajaxGetGoodsInfo();
 }
 
-/**
- * If the item is in the cart, decrement the quantity, otherwise delete the item from the cart.
- * @param item - the button that was clicked
- */
 function removeFromCart(item) {
   let dataId = item.dataset.id;
-
   if (cart[dataId] - 1 > 0) {
     cart[dataId]--;
   } else {
@@ -50,10 +35,18 @@ function removeFromCart(item) {
   ajaxGetGoodsInfo();
 }
 
-/**
- * It takes the keys of the cart object, sends them to the server, and then the server returns the data
- * associated with those keys.
- */
+function caluclationAmount(cart) {
+  let navShopAmountCart = document.querySelector(".amount_cart");
+  if (navShopAmountCart == null) {
+    return false;
+  } else {
+    let cartObj = Object.values(cart);
+    let cartArr = Array.from(cartObj);
+    let calcAmountItems = cartArr.reduce((x, y) => x + y, 0);
+    navShopAmountCart.innerHTML = calcAmountItems;
+  }
+}
+
 function ajaxGetGoodsInfo() {
   cartUpdateLocalStorage();
   fetchData("/cartTest", {
@@ -68,21 +61,10 @@ function ajaxGetGoodsInfo() {
     showCart(data);
   });
 }
-/**
- * If the cartBody and check variables are not null, then create a cardBody variable and a checkBody
- * variable, and then create a totalItemCart variable and a totalCart variable, and then for each key
- * in the cart object, add the key's value to the cardBody variable and the checkBody variable, and
- * then add the key's value to the totalCart variable and the totalItemCart variable, and then set the
- * cartBody's innerHTML to the cardBody variable and the check's innerHTML to the checkBody variable.
- * @param data - the data that is returned from the server
- * @returns the value of the variable cardBody.
- */
 
 function showCart(data) {
   let cartBody = document.querySelector(".cart-cards");
   let check = document.querySelector(".cart-check");
-  let totalAmount = document.querySelector(".amount_cart");
-
   if (cartBody == null && check == null) {
     return false;
   } else {
@@ -168,16 +150,12 @@ function showCart(data) {
           </div>
       `;
 
-    
     cartBody.innerHTML = cardBody;
     check.innerHTML = checkBody;
   }
 }
 
-/**
- * It takes the cart array and converts it to a string, then stores it in local storage.
- */
 function cartUpdateLocalStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
+  caluclationAmount(cart);
 }
-// }
--- public/js/shoppingForm.js
+++ public/js/shoppingForm.js
@@ -6,38 +6,11 @@ const inputs = document.querySelector("#form-shopping-cart").elements;
 
 const clearLocalStorage = () => {
   localStorage.removeItem("cart");
-  //   setTimeout(() => {
-  //     location.reload();
-  //     window.location.replace("/shoppingCart");
-  //   }, 3000);
+    setTimeout(() => {
+      location.reload();
+      window.location.replace("/shoppingCart");
+    }, 3000);
 };
-
-// shoppingForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   // console.log(event.target.elements);
-//   let userName = inputs[0].value.trim(),
-//     email = inputs[1].value.trim(),
-//     phoneNumber = inputs[2].value.trim(),
-//     adress = inputs[3].value.trim();
-//   console.log(userName, email, phoneNumber, adress);
-
-//   fetchData("/endOfOrder", {
-//     method: "POST",
-//     body: JSON.stringify({
-//       userName: inputs[0].value.trim(),
-//       email: inputs[1].value.trim(),
-//       phoneNumber: inputs[2].value.trim(),
-//       adress: String(inputs[3].value.trim()),
-//       key: JSON.parse(localStorage.getItem("cart")),
-//     }),
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//   }).then((response) => {
-//     console.log(response);
-//   });
-// });
 
 shoppingForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -73,38 +46,3 @@ shoppingForm.addEventListener("submit", (event) => {
     }
   });
 });
-
-//   function (event) {
-//     event.preventDefault();
-//     if (!shoppingForm.checkValidity()) {
-//       event.stopPropagation();
-//       shoppingForm.classList.add("was-validated");
-//       return;
-//     }
-//     fetchData("/endOfOrder", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         userName: inputs[0].value.trim(),
-//         email: inputs[1].value.trim(),
-//         phoneNumber: inputs[2].value.trim(),
-//         adress: String(inputs[3].value.trim()),
-//       }),
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//     }).then((response) => {
-//       if (response === 200) {
-//         Swal.fire({
-//           icon: "success",
-//         });
-//         clearLocalStorage();
-//       } else {
-//         Swal.fire({
-//           icon: "error",
-//         });
-//         clearLocalStorage();
-//       }
-//     });
-//   },
-//   false
