eb8de550a3e4fa8c933d1a76e7443a4761de888c
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2022-07-28 19:24:23 +0300

    add filter to catalog page

---
+++ public/js/productFilter.js
@@ -0,0 +1,21 @@
+const filterBox = document.querySelectorAll(".card-box");
+const filterBtns = document.querySelectorAll(".filtration-li");
+
+document.querySelector(".filtration-nav").addEventListener("click", (event) => {
+  if (event.target.tagName !== "LI") return false;
+  let filterClass = event.target.dataset["filter"];
+
+  filterBtns.forEach((elem) => {
+    if (elem.classList.contains("selected")) elem.classList.remove("selected");
+});
+    event.target.classList.add("selected");
+    
+    console.log(filterBox);
+    filterBox.forEach((elem) => {
+        elem.classList.remove("hide");
+        if (!elem.classList.contains(filterClass) && filterClass !== "all") {
+          elem.classList.add("hide");
+        }
+        console.log(elem);
+      });
+});
