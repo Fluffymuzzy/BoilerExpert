64e172099d98f5ccb5b3fa933c4ade24145496a6
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2022-08-13 15:34:21 +0300

    add animation on main page

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Sticky Navbar
+// window.addEventListener("scroll", function () {
+//   let header = document.querySelector(".header");
+//   header.classList.toggle("sticky", window.scrollY > 0);
+// });
+
+// WOW Animation
+
+new WOW().init();
+
+
